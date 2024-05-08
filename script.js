73f97e934a39ee186fca293f964b2572cf7d071a
RPapagiannis <rpapagiannis@icloud.com>
2024-05-08 14:31:57 +0300

    Extend game to span multiple rounds
    Added a while loop that ensure that the game keeps running until the
    player or the computer reaches 5 points.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt('Rock, Paper, or Scissors?')
-    return humanChoice.toLowerCase; 
+    return humanChoice;
 }
 
 function playRound(computerChoice, humanChoice) {
@@ -26,16 +26,50 @@ function playRound(computerChoice, humanChoice) {
         console.log('The humie wins!')
         return humanScore ++;
     }
+    else if (computerChoice == 'rock' && humanChoice == 'scissors') {
+        console.log('The computer wins!')
+        return computerScore ++;
+    }
+    else if (computerChoice == 'paper' && humanChoice == 'rock') {
+        console.log('The computer wins!')
+        return computerScore ++;
+    }
+    else if (computerChoice == 'paper' && humanChoice == 'paper') {
+        console.log('Draw!')
+    }
+    else if (computerChoice == 'paper' && humanChoice == 'scissors') {
+        console.log('The humie wins!')
+        return humanScore ++;
+    }
+    else if (computerChoice == 'scissors' && humanChoice == 'rock') {
+        console.log('The humie wins!')
+        return humanScore ++;
+    }
+    else if (computerChoice == 'scissors' && humanChoice == 'paper') {
+        console.log('The computer wins!')
+        return computerScore ++;
+    }
+    else if (computerChoice == 'scissors' && humanChoice == 'scissors') {
+        console.log('Draw!')
+    }
+    else {
+        console.log('Invalid input')
+    }
 }
 
 let humanScore = 0;
 
 let computerScore = 0;
 
-let computerChoice = getComputerChoice();
-
-let humanChoice = getHumanChoice();
-
-playRound(computerChoice, humanChoice);
-
-console.log(humanScore);
+while (humanScore < 5 && computerScore < 5) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice().toLowerCase();
+    playRound(computerChoice, humanChoice);
+    console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+    if (humanScore === 5) {
+        console.log('The human wins the game!')
+    }
+    else if (computerScore === 5) {
+        console.log('The computer wins the game!')
+    }
+}
